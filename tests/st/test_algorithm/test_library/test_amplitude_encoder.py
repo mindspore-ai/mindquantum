6f8c5e68ae6015e150cf2e4e621d8f479ae962cb
SugarSBN <bokkenasu@163.com>
2021-12-20 04:38:04 +0000

    update tests/st/test_algorithm/test_library/test_amplitude_encoder.py.

--- tests/st/test_algorithm/test_library/test_amplitude_encoder.py
+++ tests/st/test_algorithm/test_library/test_amplitude_encoder.py
@@ -20,12 +20,14 @@ from mindquantum.simulator import Simulator
 def test_amplitude_encoder():
     '''test for amplitude encoder'''
     sim = Simulator('projectq', 3)
-    circuit, params = amplitude_encoder([0.5, 0.5, 0.5, 0.5])
+    circuit, params = amplitude_encoder([0.5, 0.5, 0.5, 0.5], 3)
     sim.apply_circuit(circuit, params)
     st = sim.get_qs(False)
-    for i in range(8):
-        assert abs(st[i].real - (0.5 if i < 4 else 0)) < 1e-10
-    circuit, params = amplitude_encoder([0, 0, 0.5, 0.5, 0.5, 0.5])
+    assert abs(st[0].real - 0.5) < 1e-10
+    assert abs(st[2].real - 0.5) < 1e-10
+    assert abs(st[4].real - 0.5) < 1e-10
+    assert abs(st[6].real - 0.5) < 1e-10
+    circuit, params = amplitude_encoder([0, 0, 0.5, 0.5, 0.5, 0.5], 3)
     sim.reset()
     sim.apply_circuit(circuit, params)
     st = sim.get_qs(False)
