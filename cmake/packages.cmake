4f5a6341cfd76d655f66a0f479e49b85dbeeb365
Damien Nguyen <damien1@huawei.com>
2022-02-08 14:25:13 +0100

    Fix finding Python in CMake

--- cmake/packages.cmake
+++ cmake/packages.cmake
@@ -82,7 +82,13 @@ if(DEFINED PYTHON_EXECUTABLE)
   set(Python_EXECUTABLE ${PYTHON_EXECUTABLE}) # cmake-lint: disable=C0103
 endif()
 
-find_package(Python 3.5.0 COMPONENTS Interpreter Development)
+set(_python_find_args Python 3.5.0 COMPONENTS Interpreter)
+if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.18)
+  list(APPEND _python_find_args Development.Module)
+else()
+  list(APPEND _python_find_args Development)
+endif()
+find_package(${_python_find_args})
 
 # NB: This should be removed for CMake >= 3.16
 if(NOT Python_FOUND)
