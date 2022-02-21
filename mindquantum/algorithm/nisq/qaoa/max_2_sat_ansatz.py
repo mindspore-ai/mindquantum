e4b4d0d69166eb415824dacef3080f1915de694e
zhou-xu3 <zhoux229@mail2.sysu.edu.cn>
2022-02-21 17:08:32 +0800

    max2sat

--- mindquantum/algorithm/nisq/qaoa/max_2_sat_ansatz.py
+++ mindquantum/algorithm/nisq/qaoa/max_2_sat_ansatz.py
@@ -16,8 +16,13 @@
 """Max-2-SAT ansatz."""
 
 from math import copysign as sign
+import numpy as np
 from mindquantum.core.circuit import Circuit, UN
 from mindquantum.core.circuit.utils import CPN
+from mindquantum.simulator import Simulator
+from mindquantum.utils.type_value_check import _check_int_type
+from mindquantum.utils.type_value_check import _check_value_should_between_close_set
+from mindquantum.utils.type_value_check import _check_input_type
 from mindquantum.core.gates import H, RX
 from mindquantum.core.operators import TimeEvolution, QubitOperator
 from .._ansatz import Ansatz
@@ -40,19 +45,16 @@ def _get_clause_act_qubits(clauses):
 
 
 def _check_clause(clauses):
-    """check clause"""
+    """check clauses"""
     if not isinstance(clauses, list):
         raise TypeError(f"clauses requires a list, but get {type(clauses)}")
     for clause in clauses:
         if not isinstance(clause, tuple):
             raise TypeError(f"clause requires a tuple, but get {type(clause)}")
         if len(clause) != 2:
-            raise ValueError(
-                f"each clause must contain two integers, but get {len(clause)}"
-            )
+            raise ValueError(f"each clause must contain two integers, but get {len(clause)}")
         if 0 in clause:
-            raise ValueError(
-                "clause must contain non-zero integers, but get 0")
+            raise ValueError("clause must contain non-zero integers, but get 0")
 
 
 class Max2SATAnsatz(Ansatz):
@@ -75,7 +77,7 @@ class Max2SATAnsatz(Ansatz):
     of total clauses and :math:`P(l)` is rank-one projector.
 
     Args:
-        graph (list[tuple[int]]): The Max-2-SAT structure. Every element of list
+        clauses (list[tuple[int]]): The Max-2-SAT structure. Every element of list
             is a clause represented by a tuple with length two. The element of
             tuple must be non-zero integer. For example, (2, -3) stands for clause
             :math:`x_2\lor\lnot x_3`.
@@ -95,6 +97,15 @@ class Max2SATAnsatz(Ansatz):
         0.25 [Z1] +
         -0.25 [Z1 Z2] +
         -0.25 [Z2]
+        >>> sats = max2sat.get_sat(4, np.array([4, 1]))
+        >>> sats
+        ['001', '000', '011', '010']
+        >>> for i in sats:
+        >>>     print(f'sat value: {max2sat.get_sat_value([i])}')
+        sat value: 1
+        sat value: 0
+        sat value: 2
+        sat value: 1
     """
     def __init__(self, clauses, depth=1):
         if not isinstance(depth, int):
@@ -102,9 +113,7 @@ class Max2SATAnsatz(Ansatz):
         if depth <= 0:
             raise ValueError(f"depth must be greater than 0, but get {depth}.")
         _check_clause(clauses)
-        super(Max2SATAnsatz,
-              self).__init__('Max2SAT', _get_clause_act_qubits_num(clauses),
-                             clauses, depth)
+        super(Max2SATAnsatz, self).__init__('Max2SAT', _get_clause_act_qubits_num(clauses), clauses, depth)
         self.clauses = clauses
         self.depth = depth
 
@@ -112,44 +121,71 @@ class Max2SATAnsatz(Ansatz):
         """Build hc circuit."""
         ham = QubitOperator()
         for clause in clauses:
-            ham += (sign(1, clause[0]) * QubitOperator(
-                f'Z{abs(clause[0]) - 1}', 'beta') + sign(1, clause[1]) *
-                    QubitOperator(f'Z{abs(clause[1]) - 1}', 'beta') +
-                    sign(1, clause[0]) * sign(1, clause[1]) * QubitOperator(
-                        f'Z{abs(clause[0]) - 1} Z{abs(clause[1]) - 1}', 'beta')
-                    ) / 4
+            ham += (sign(1, clause[0]) * QubitOperator(f'Z{abs(clause[0]) - 1}', 'beta') +
+                    sign(1, clause[1]) * QubitOperator(f'Z{abs(clause[1]) - 1}', 'beta')) / 4
+            ham += (sign(1, clause[0]) * sign(1, clause[1]) *
+                    QubitOperator(f'Z{abs(clause[0]) - 1} Z{abs(clause[1]) - 1}', 'beta')) / 4
         return TimeEvolution(ham).circuit
 
     def _build_hb(self, clauses):
         """Build hb circuit."""
-        circ = Circuit(
-            [RX('alpha').on(i) for i in _get_clause_act_qubits(clauses)])
+        circ = Circuit([RX('alpha').on(i) for i in _get_clause_act_qubits(clauses)])
         return circ
 
     @property
     def hamiltonian(self):
         """
-        Get the hamiltonian of this max 2 sat problem.
+        Get the hamiltonian of this max-2-sat problem.
 
         Returns:
-            QubitOperator, hamiltonian of this max 2 sat problem.
+            QubitOperator, hamiltonian of this max-2-sat problem.
         """
         qo = QubitOperator()
         for clause in self.clauses:
-            qo += (
-                QubitOperator('') +
-                sign(1, clause[0]) * QubitOperator(f'Z{abs(clause[0]) - 1}') +
-                sign(1, clause[1]) * QubitOperator(f'Z{abs(clause[1]) - 1}') +
-                sign(1, clause[0]) * sign(1, clause[1]) *
-                QubitOperator(f'Z{abs(clause[0]) - 1} Z{abs(clause[1]) - 1}')
-            ) / 4
+            qo += (QubitOperator('') + sign(1, clause[0]) * QubitOperator(f'Z{abs(clause[0]) - 1}') +
+                   sign(1, clause[1]) * QubitOperator(f'Z{abs(clause[1]) - 1}') + sign(1, clause[0]) *
+                   sign(1, clause[1]) * QubitOperator(f'Z{abs(clause[0]) - 1} Z{abs(clause[1]) - 1}')) / 4
         return qo
 
+    def get_sat(self, max_n, weight):
+        """
+            Get the strings of this max-2-sat problem.
+
+            Args:
+                max_n (int): how many strings you want.
+                weight (Union[ParameterResolver, dict, numpy.ndarray, list, numbers.Number]): parameter
+                    value for Max-2-SAT ansatz.
+
+            Returns:
+                list, a list of strings.
+        """
+        _check_int_type('max_n', max_n)
+        _check_value_should_between_close_set('max_n', 1, 1 << self._circuit.n_qubits, max_n)
+        sim = Simulator('projectq', self._circuit.n_qubits)
+        sim.apply_circuit(self._circuit, weight)
+        qs = sim.get_qs()
+        idxs = np.argpartition(np.abs(qs), -max_n)[-max_n:]
+        strings = [bin(i)[2:].zfill(self._circuit.n_qubits)[::-1] for i in idxs]
+        return strings
+
+    def get_sat_value(self, string):
+        """
+        Get the sat values for given strings.
+        The string is a str that satisfies all the clauses of the given max-2-sat problem.
+
+        Args:
+            string (str): a string of the max-2-sat problem consided.
+
+        Returns:
+            int, sat_value under the given string.
+        """
+        _check_input_type('string', str, string)
+        sat_value = string.count('1')
+        return sat_value
+
     def _implement(self, clauses, depth):
-        """Implement of max 2 sat ansatz."""
+        """Implement of Max-2-SAT ansatz."""
         self._circuit = UN(H, _get_clause_act_qubits(clauses))
         for d in range(depth):
-            self._circuit += CPN(self._build_hc(clauses),
-                                 {'beta': f'beta_{d}'})
-            self._circuit += CPN(self._build_hb(clauses),
-                                 {'alpha': f'alpha_{d}'})
+            self._circuit += CPN(self._build_hc(clauses), {'beta': f'beta_{d}'})
+            self._circuit += CPN(self._build_hb(clauses), {'alpha': f'alpha_{d}'})
