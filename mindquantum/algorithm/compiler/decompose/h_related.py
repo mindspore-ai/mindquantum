b0a3de7d972993375ffd43295dee2f378b5e8d9f
Arapat Ablimit <aablimit@126.com>
2022-05-11 15:12:21 +0000

    y, z, rx, ry, rz, h, swap related

---
+++ mindquantum/algorithm/compiler/decompose/h_related.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""
+H gate related decompose rule.
+"""
+
+from mindquantum.core import gates as G
+from mindquantum.core import Circuit, T, X, H, S
+from mindquantum.utils.type_value_check import _check_input_type, _check_control_num
+
+def ch_decompose(gate: G.H):
+    """
+    Decompose ch gate.
+
+    Args:
+        gate (H): a H gate with one control qubits.
+
+    Returns:
+        List[Circuit], all possible decompose solution.
+
+    Examples:
+        >>> from mindquantum.algorithm.compiler.decompose import ch_decompose
+        >>> from mindquantum.core import Circuit, T, X, H, S
+        >>> ch = H.on(1, 0)
+        >>> origin_circ = Circuit() + ch
+        >>> decomposed_circ = ch_decompose(ch)[0]
+        >>> origin_circ
+        q0: ──●──
+              │
+        q1: ──H──
+
+        >>> decomposed_circ
+        q0: ─────────────────●───────────────────
+                                │
+        q1: ──S────H────T────X────T†────H────S†──
+    """
+    
+    _check_input_type('gate', G.H, gate)
+    _check_control_num(gate.ctrl_qubits, 1)
+    solutions = []
+    c1 = Circuit()
+    solutions.append(c1)
+    q0 = gate.obj_qubits[0]
+    q1 = gate.ctrl_qubits[0]
+    
+    c1 += S.on(q0)
+    c1 += H.on(q0)
+    c1 += T.on(q0)
+    c1 += X.on(q0,q1)
+    c1 += T.on(q0).hermitian()
+    c1 += H.on(q0)
+    c1 += S.on(q0).hermitian()
+    
+    return solutions
+
+decompose_rules = ['ch_decompose']
+__all__ = decompose_rules
---
+++ mindquantum/algorithm/compiler/decompose/swap_related.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""
+SWAP gate related decompose rule.
+"""
+
+from mindquantum.core import gates as G
+from mindquantum.core import Circuit, X, SWAP
+from mindquantum.utils.type_value_check import _check_input_type, _check_control_num
+
+def swap_decompose(gate: G.SWAP):
+    """
+    Decompose swap gate.
+
+    Args:
+        gate (SWAP): a SWAP gate.
+
+    Returns:
+        List[Circuit], all possible decompose solution.
+
+    Examples:
+        >>> from mindquantum.algorithm.compiler.decompose import swap_decompose
+        >>> from mindquantum.core import Circuit, SWAP
+        >>> swap = SWAP.on([1, 0])
+        >>> origin_circ = Circuit() + swap
+        >>> decomposed_circ = swap_decompose(swap)[0]
+        >>> origin_circ
+        q0: ──@──
+              │
+        q1: ──@──
+
+        >>> decomposed_circ
+        q0: ──X────●────X──
+              │     │    │
+        q1: ──●────X────●──
+    """
+    
+    _check_input_type('gate', G.SWAP, gate)
+    _check_control_num(gate.obj_qubits, 2)
+    solutions = []
+    c1 = Circuit()
+    solutions.append(c1)
+    q0 = gate.obj_qubits[0]
+    q1 = gate.obj_qubits[1]
+    
+    c1 += G.X.on(q1,q0)
+    c1 += G.X.on(q0,q1)
+    c1 += G.X.on(q1,q0)
+    
+    return solutions
+
+def cswap_decompose(gate: G.SWAP):
+    """
+    Decompose cswap gate.
+
+    Args:
+        gate (SWAP): a SWAP gate with one control qubit.
+
+    Returns:
+        List[Circuit], all possible decompose solution.
+
+    Examples:
+        >>> from mindquantum.algorithm.compiler.decompose import cswap_decompose
+        >>> from mindquantum.core import Circuit, SWAP
+        >>> swap = SWAP.on([1, 2], 0)
+        >>> origin_circ = Circuit() + cswap
+        >>> decomposed_circ = cswap_decompose(swap)[0]
+        >>> origin_circ
+        q0: ──●──
+              │
+        q1: ──@──
+              │
+        q2: ──@──
+
+        >>> decomposed_circ
+        q0: ───────●───────
+                │
+        q1: ──X────●────X──
+            │    │    │
+        q2: ──●────X────●──
+    """
+    
+    _check_input_type('gate', G.SWAP, gate)
+    _check_control_num(gate.ctrl_qubits, 1)
+    solutions = []
+    c1 = Circuit()
+    solutions.append(c1)
+    q0 = gate.ctrl_qubits[0]
+    q1 = gate.obj_qubits[0]
+    q2 = gate.obj_qubits[1]
+    
+    c1 += X.on(q1,q2)
+    c1 += X.on(q2,[q0,q1])
+    c1 += X.on(q1,q2)
+    
+    return solutions
+
+decompose_rules = ['swap_decompose', 'cswap_decompose']
+__all__ = decompose_rules 
