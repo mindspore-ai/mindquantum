51f512b1dbbd33a41272ec003321b2e8c478114e
Arapat Ablimit <aablimit@126.com>
2022-05-13 15:47:36 +0000

    update tests/st/test_algorithm/test_compiler/test_decompose/test_ry_decompose.py.

--- tests/st/test_algorithm/test_compiler/test_decompose/test_ry_decompose.py
+++ tests/st/test_algorithm/test_compiler/test_decompose/test_ry_decompose.py
@@ -14,7 +14,6 @@
 # limitations under the License.
 # ============================================================================
 '''test decompose rule'''
-import numpy as np
 from mindquantum.algorithm.compiler.decompose import cry_decompose
 from mindquantum.core import Circuit
 from mindquantum.core import RY
