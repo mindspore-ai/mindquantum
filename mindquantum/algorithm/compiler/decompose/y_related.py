7bcbd0101c7b631fa0847c03b99e62135b9c5703
Arapat Ablimit <aablimit@126.com>
2022-05-13 09:27:32 +0000

    update mindquantum/algorithm/compiler/decompose/y_related.py.

--- mindquantum/algorithm/compiler/decompose/y_related.py
+++ mindquantum/algorithm/compiler/decompose/y_related.py
@@ -18,7 +18,7 @@ y gate related decompose rule.
 """
 
 from mindquantum.core import gates as G
-from mindquantum.core import Circuit, RY, X, Y, S
+from mindquantum.core import Circuit
 from mindquantum.utils.type_value_check import _check_input_type, _check_control_num
 
 def cy_decompose(gate: G.Y):
@@ -47,7 +47,6 @@ def cy_decompose(gate: G.Y):
                     │
         q1: ──S†────X────S──
     """
-    
     _check_input_type('gate', G.Y, gate)
     _check_control_num(gate.ctrl_qubits, 1)
     solutions = []
@@ -55,11 +54,9 @@ def cy_decompose(gate: G.Y):
     solutions.append(c1)
     q0 = gate.obj_qubits[0]
     q1 = gate.ctrl_qubits[0]
-    
     c1 += G.S.on(q0).hermitian()
     c1 += G.X.on(q0, q1)
     c1 += G.S.on(q0)
-    
     return solutions
 
 decompose_rules = ['cy_decompose']
