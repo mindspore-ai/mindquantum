19414559ece7051c8d4b64412fb651c2c24c46cf
Damien Nguyen <damien1@huawei.com>
2022-08-10 14:27:34 +0200

    [CXX] Add unit tests for QubitOperator

--- tests/ops/test_qubit_operators.cpp
+++ tests/ops/test_qubit_operators.cpp
@@ -12,32 +12,39 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
-#include <complex>
-#include <string>
-#include <string_view>
-
 #include "core/logging.hpp"
 #include "ops/gates/qubit_operator.hpp"
 #include "ops/utils.hpp"
 
+// clang-format off
 #include <catch2/catch.hpp>
+// clang-format on
 
 // =============================================================================
 
 namespace ops = mindquantum::ops;
-using namespace std::literals::complex_literals;
-using namespace std::literals::string_literals;
 
+using namespace std::literals::complex_literals;
 using QubitOperator = ops::QubitOperator;
 using TermValue = ops::TermValue;
 using coefficient_t = QubitOperator::coefficient_t;
 using term_t = QubitOperator::term_t;
 using terms_t = QubitOperator::terms_t;
-using coeff_term_dict_t = QubitOperator::coeff_term_dict_t;
+using complex_term_dict_t = QubitOperator::complex_term_dict_t;
+
+// -----------------------------------------------------------------------------
+
+class UnitTestAccessor {
+ public:
+    static auto parse_string(std::string_view terms_string) {
+        return QubitOperator::parse_string_(terms_string);
+    }
+};
 
 // =============================================================================
 
 TEST_CASE("QubitOperator parse_string", "[terms_op][ops]") {
+    MQ_DISABLE_LOGGING;
     std::string terms_string;
     terms_t ref_terms;
 
@@ -95,17 +102,14 @@ TEST_CASE("QubitOperator parse_string", "[terms_op][ops]") {
         ref_terms.emplace_back(1, TermValue::X);
     }
 
-    const auto terms = QubitOperator::term_policy_t::parse_terms_string(terms_string);
+    const auto terms = UnitTestAccessor::parse_string(terms_string);
 
-    INFO("terms_string = " << terms_string);
     REQUIRE(std::size(ref_terms) == std::size(terms));
-
-    INFO("terms_string = " << terms_string);
     CHECK(ref_terms == terms);
 }
 
 TEST_CASE("QubitOperator constructor", "[qubit_op][ops]") {
-    auto ref_terms = coeff_term_dict_t{{{{1, TermValue::X}, {2, TermValue::Y}, {4, TermValue::Z}}, 1.}};
+    auto ref_terms = complex_term_dict_t{{{{1, TermValue::X}, {2, TermValue::Y}, {4, TermValue::Z}}, 1.}};
 
     QubitOperator op("X1 Y2 Z4");
     CHECK(!std::empty(op));
@@ -122,50 +126,6 @@ TEST_CASE("QubitOperator constructor", "[qubit_op][ops]") {
     CHECK(QubitOperator("XX").is_identity());
     CHECK(QubitOperator("1X").is_identity());
     CHECK(QubitOperator("Y1 Z2 1X Y3").is_identity());
-}
-
-TEST_CASE("QubitOperator split", "[terms_op][ops]") {
-    const auto lhs = QubitOperator("X1", 1.2i);
-    const auto rhs = QubitOperator("Z3", 1.2);
-    const auto qubit_op = lhs + rhs;
-
-    const auto splitted = qubit_op.split();
-    REQUIRE(std::size(splitted) == 2);
-    if (splitted[0] == lhs) {
-        CHECK(splitted[0] == lhs);
-        CHECK(splitted[1] == rhs);
-    } else {
-        CHECK(splitted[0] == rhs);
-        CHECK(splitted[1] == lhs);
-    }
-}
-
-TEST_CASE("QubitOperator comparison operators", "[terms_op][ops]") {
-    coeff_term_dict_t ref_terms;
-
-    auto [it1, inserted1] = ref_terms.emplace(terms_t{{3, TermValue::X}}, 2.3);
-    auto [it2, inserted2] = ref_terms.emplace(terms_t{{1, TermValue::Y}}, 1.);
-    REQUIRE(inserted1);
-    REQUIRE(inserted2);
-
-    // op = {'3': 2.3,  '1^': 1.}
-    const QubitOperator qubit_op(ref_terms);
-    QubitOperator other(ref_terms);
-
-    CHECK(qubit_op == qubit_op);
-    CHECK(qubit_op == other);
-
-    SECTION("Add identity term") {
-        other += QubitOperator::identity();
-    }
-    SECTION("Add other term") {
-        other += QubitOperator{terms_t{{2, TermValue::Z}}, 2.34i};
-    }
-    SECTION("No common terms") {
-        other = QubitOperator{terms_t{{2, TermValue::Z}}, 2.34i};
-    }
-    CHECK(!(qubit_op == other));
-    CHECK(qubit_op != other);
 }
 
 // =============================================================================
