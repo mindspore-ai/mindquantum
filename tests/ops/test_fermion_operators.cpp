cfefb43b9d2c1c7c31e9743f6995f39efe55ff3b
Damien Nguyen <damien1@huawei.com>
2022-08-10 14:14:23 +0200

    [CXX] Add unit tests for FermionOperator

--- tests/ops/test_fermion_operators.cpp
+++ tests/ops/test_fermion_operators.cpp
@@ -13,28 +13,41 @@
 //   limitations under the License.
 
 #include <complex>
+#include <sstream>
 #include <string>
-#include <string_view>
+
+#include <boost/range/combine.hpp>
 
 #include "core/circuit_block.hpp"
 #include "core/logging.hpp"
 #include "ops/gates/fermion_operator.hpp"
+#include "ops/gates/qubit_operator.hpp"
 #include "ops/utils.hpp"
 
+// clang-format off
 #include <catch2/catch.hpp>
+// clang-format on
 
 // =============================================================================
 
 namespace ops = mindquantum::ops;
-using namespace std::literals::complex_literals;
-using namespace std::literals::string_literals;
 
+using namespace std::literals::complex_literals;
 using FermionOperator = ops::FermionOperator;
 using TermValue = mindquantum::ops::TermValue;
 using coefficient_t = FermionOperator::coefficient_t;
 using term_t = FermionOperator::term_t;
 using terms_t = FermionOperator::terms_t;
-using coeff_term_dict_t = FermionOperator::coeff_term_dict_t;
+using complex_term_dict_t = FermionOperator::complex_term_dict_t;
+
+// -----------------------------------------------------------------------------
+
+class UnitTestAccessor {
+ public:
+    static auto parse_string(std::string_view terms_string) {
+        return FermionOperator::parse_string_(terms_string);
+    }
+};
 
 // =============================================================================
 
@@ -92,116 +105,10 @@ TEST_CASE("FermionOperator parse_string", "[terms_op][ops]") {
         ref_terms.emplace_back(2, TermValue::a);
     }
 
-    const auto terms = FermionOperator::term_policy_t::parse_terms_string(terms_string);
+    const auto terms = UnitTestAccessor::parse_string(terms_string);
 
-    INFO("terms_string = " << terms_string);
     REQUIRE(std::size(ref_terms) == std::size(terms));
-
-    INFO("terms_string = " << terms_string);
     CHECK(ref_terms == terms);
-}
-
-// -----------------------------------------------------------------------------
-
-TEST_CASE("FermionOperator constructor", "[terms_op][ops]") {
-    const auto coeff = 2.34i;
-    auto ref_terms = coeff_term_dict_t{{{{1, TermValue::a}, {2, TermValue::adg}, {4, TermValue::a}}, coeff}};
-
-    FermionOperator fermion_op("1 2^ 4", coeff);
-    CHECK(!std::empty(fermion_op));
-    CHECK(std::size(fermion_op) == 1);
-    CHECK(fermion_op.get_terms() == ref_terms);
-
-    const auto [it, inserted] = ref_terms.emplace(terms_t{{1, TermValue::adg}}, 3.2);
-    fermion_op += FermionOperator("1^", it->second);
-
-    CHECK(std::size(fermion_op) == 2);
-    CHECK(fermion_op.get_terms() == ref_terms);
-
-    // NB: failure to parse will result in an empty list of terms... -> identity()
-    CHECK(FermionOperator("1^^").is_identity());
-    CHECK(FermionOperator("^").is_identity());
-    CHECK(FermionOperator("2 1^ 11a 3").is_identity());
-}
-
-TEST_CASE("FermionOperator split", "[terms_op][ops]") {
-    const auto lhs = FermionOperator("1^", 1.2i);
-    const auto rhs = FermionOperator("0", 1.2);
-    const auto fermion_op = lhs + rhs;
-
-    const auto splitted = fermion_op.split();
-    REQUIRE(std::size(splitted) == 2);
-    if (splitted[0] == lhs) {
-        CHECK(splitted[0] == lhs);
-        CHECK(splitted[1] == rhs);
-    } else {
-        CHECK(splitted[0] == rhs);
-        CHECK(splitted[1] == lhs);
-    }
-}
-
-TEST_CASE("FermionOperator normal_ordered", "[terms_op][ops]") {
-    const auto coeff = 2.34i;
-    auto ref_coeff = coeff;
-
-    std::string fermion_op_str;
-    FermionOperator ref_op;
-
-    SECTION("1 0") {
-        ref_op = FermionOperator("1 0", ref_coeff);
-        fermion_op_str = "1 0";
-    }
-    SECTION("0 1") {
-        ref_coeff *= -1;
-        ref_op = FermionOperator("1 0", ref_coeff);
-        fermion_op_str = "0 1";
-    }
-    SECTION("0 1^") {
-        ref_coeff *= -1;
-        ref_op = FermionOperator("1^ 0", ref_coeff);
-        fermion_op_str = "0 1^";
-    }
-    SECTION("1 0 2") {
-        ref_op = FermionOperator("2 1 0", ref_coeff);
-        fermion_op_str = "1 0 2";
-    }
-    SECTION("1 4 1^") {
-        ref_coeff *= -1;
-        ref_op = FermionOperator("1^ 4 1", ref_coeff) + FermionOperator("4", ref_coeff);
-        fermion_op_str = "1 4 1^";
-    }
-
-    const auto normal_ordered = FermionOperator(fermion_op_str, coeff).normal_ordered();
-    INFO("fermion_op = FermionOperator(\"" << fermion_op_str << "\")");
-    CHECK(normal_ordered == ref_op);
-}
-
-TEST_CASE("FermionOperator comparison operators", "[terms_op][ops]") {
-    coeff_term_dict_t ref_terms;
-
-    auto [it1, inserted1] = ref_terms.emplace(terms_t{{3, TermValue::a}}, 2.3);
-    auto [it2, inserted2] = ref_terms.emplace(terms_t{{1, TermValue::adg}}, 1.);
-    REQUIRE(inserted1);
-    REQUIRE(inserted2);
-
-    // op = {'3': 2.3,  '1^': 1.}
-    const FermionOperator op(ref_terms);
-    FermionOperator other(ref_terms);
-
-    CHECK(op == op);
-    CHECK(op == other);
-
-    SECTION("Add identity term") {
-        other += FermionOperator::identity();
-    }
-    SECTION("Add other term") {
-        other += FermionOperator{terms_t{{2, TermValue::a}}, 2.34i};
-    }
-    SECTION("No common terms") {
-        other = FermionOperator{terms_t{{2, TermValue::a}}, 2.34i};
-    }
-    CHECK(!(op == other));
-    CHECK(op != other);
 }
 
 // =============================================================================
