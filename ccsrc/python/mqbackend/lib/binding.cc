4ce761235296a5640bc0ef33e02fe93aeae51986
Damien Nguyen <damien1@huawei.com>
2022-09-05 11:51:35 +0200

    [CXX] Cleanup code for mqbackend bindings

--- ccsrc/python/mqbackend/lib/binding.cc
+++ ccsrc/python/mqbackend/lib/binding.cc
@@ -19,6 +19,8 @@
 #include <pybind11/stl.h>
 #include <pybind11/stl_bind.h>
 
+#include "ops/basic_gate.hpp"
+
 #ifdef ENABLE_PROJECTQ
 #    include "projectq.h"
 #endif
@@ -36,7 +38,6 @@
 
 namespace py = pybind11;
 
-namespace mindquantum {
 using mindquantum::sparse::Csr_Plus_Csr;
 using mindquantum::sparse::GetPauliMat;
 using mindquantum::sparse::PauliMat;
@@ -49,8 +50,70 @@ using mindquantum::projectq::InnerProduct;
 using mindquantum::projectq::Projectq;
 #endif
 
+void BindPRPython(py::module *m) {
+    using mindquantum::MST;
+    using mindquantum::PRPython;
+    using mindquantum::PRTypeID;
+
+    py::class_<PRPython, std::shared_ptr<PRPython>>(*m, "pr_python")
+        .def(py::init<double>())
+        .def(py::init<std::complex<double>>())
+        .def(py::init<const MST<double>, double>())
+        .def(py::init<const MST<std::complex<double>>, std::complex<double>>())
+        .def(py::init([](const PRPython &pr) { return std::make_shared<PRPython>(pr); }))
+        .def(py::init<std::string, PRTypeID>())
+        .def("set_const", py::overload_cast<double>(&PRPython::SetConst))
+        .def("set_const", py::overload_cast<std::complex<double>>(&PRPython::SetConst))
+        .def("params_name", &PRPython::ParamsName)
+        .def("display", &PRPython::PrintInfo)
+        .def("__len__", &PRPython::Size)
+        .def("size", &PRPython::Size)
+        .def("__bool__", &PRPython::IsNotZero)
+        .def("__repr__", &PRPython::ToString)
+        .def("__str__", &PRPython::ToString)
+        .def("__contains__", &PRPython::Contains)
+        .def("__copy__", [](const PRPython &pr) { return pr; })
+        .def("get_key", &PRPython::GetKey)
+        .def(py::self += double(), py::is_operator())
+        .def(py::self += std::complex<double>(), py::is_operator())
+        .def(py::self += py::self, py::is_operator())
+        .def(py::self -= double(), py::is_operator())
+        .def(py::self -= std::complex<double>(), py::is_operator())
+        .def(py::self -= py::self, py::is_operator())
+        .def(py::self *= double(), py::is_operator())
+        .def(py::self *= std::complex<double>(), py::is_operator())
+        .def(py::self *= py::self, py::is_operator())
+        .def(py::self /= double(), py::is_operator())
+        .def(py::self /= std::complex<double>(), py::is_operator())
+        .def(py::self /= py::self, py::is_operator())
+        .def("is_const", &PRPython::IsConst)
+        .def("is_const", &PRPython::IsConst)
+        .def("requires_grad", &PRPython::RequiresGrad)
+        .def("no_grad", &PRPython::NoGrad)
+        .def("no_grad_part", &PRPython::NoGradPart)
+        .def("requires_grad_part", &PRPython::RequiresGradPart)
+        .def("as_encoder", &PRPython::AsEncoder)
+        .def("as_ansatz", &PRPython::AsAnsatz)
+        .def("encoder_part", &PRPython::EncoderPart)
+        .def("ansatz_part", &PRPython::AnsatzPart)
+        .def("update", py::overload_cast<const PRPython &>(&PRPython::Update))
+        .def("conjugate", &PRPython::Conjugate)
+        .def("combination", &PRPython::Combination)
+        .def("real", &PRPython::Real)
+        .def("imag", &PRPython::Imag)
+        // .def("pop", &PRPython::Pop)
+        .def("is_hermitian", &PRPython::IsHermitian)
+        .def("is_anti_hermitian", &PRPython::IsAntiHermitian)
+        .def("to_complex", &PRPython::ToComplexPR)
+        .def("is_complex_pr", &PRPython::IsComplexPR);
+}
+
 template <typename T>
 void BindPR(py::module *m, const std::string &name) {
+    using mindquantum::MST;
+    using mindquantum::ParameterResolver;
+    using mindquantum::SS;
+
     py::class_<ParameterResolver<T>, std::shared_ptr<ParameterResolver<T>>>(*m, name.c_str())
         .def(py::init<T>())
         .def(py::init([](ParameterResolver<T> &pr, bool copy) {
@@ -118,6 +181,21 @@ void BindPR(py::module *m, const std::string &name) {
 
 // Interface with python
 PYBIND11_MODULE(mqbackend, m) {
+    using mindquantum::BasicGate;
+    using mindquantum::CsrHdMatrix;
+    using mindquantum::CT;
+    using mindquantum::Dim2Matrix;
+    using mindquantum::GetGateByName;
+    using mindquantum::GetMeasureGate;
+    using mindquantum::Hamiltonian;
+    using mindquantum::Index;
+    using mindquantum::MT;
+    using mindquantum::ParameterResolver;
+    using mindquantum::PauliTerm;
+    using mindquantum::VS;
+    using mindquantum::VT;
+    using mindquantum::VVT;
+
     m.doc() = "MindQuantum c plugin";
 
     // matrix
@@ -223,4 +301,3 @@ PYBIND11_MODULE(mqbackend, m) {
     m.def("cpu_projectq_inner_product", &InnerProduct<MT>);
 #endif
 }
-}  // namespace mindquantum
