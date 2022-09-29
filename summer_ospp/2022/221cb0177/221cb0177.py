3b0b2bc4e125af3ca0daa6b886844b25d2d666b3
86182 <18264633587@163.com>
2022-09-29 21:41:41 +0800

    modified by nxh

--- summer_ospp/2022/221cb0177/221cb0177.py
+++ summer_ospp/2022/221cb0177/221cb0177.py
@@ -36,8 +36,8 @@ InteractiveShell.ast_node_interactivity = 'all'
 
 # 创建p层ansatz电路，直接调用HardwareEffiecient ansatz
 # 搭建含参酉电路U（lamada），制备量子态phi(lanmada) = U(lamada)|0>
-def create_ansatz_circuit(n,p):
-    ansatz_p = HardwareEfficientAnsatz(n, single_rot_gate_seq=[RY], entangle_gate=X, depth=p).circuit     # 通过HardwareEfficientAnsatz搭建Ansatz
+def create_ansatz_circuit(n,layer):
+    ansatz_p = HardwareEfficientAnsatz(n, single_rot_gate_seq=[RY], entangle_gate=X, depth=layer).circuit     # 通过HardwareEfficientAnsatz搭建Ansatz
     ansatz_p.as_ansatz()
     return  ansatz_p
 
@@ -45,7 +45,10 @@ def create_ansatz_circuit(n,p):
 # In[3]:
 
 
-# 获取当前量子态信息,存储在字典info中，info = {'十进制k':phi_k},k = 0,1,2...N-1
+# 参数：一维数组 ansatz_data，为含参酉电路赋值；
+# 功能：获取ψ(λ)中的测量结果；
+# 返回：字典info，存储十进制k和系数ψ_k之间的对应关系；
+
 def get_info_state(n,layer,ansatz_data): 
     # 获取制备量子态的含参酉电路
     ansatz= create_ansatz_circuit(n,layer)
@@ -183,12 +186,54 @@ def calculate_P(n,layer,ansatz_data,info):
 # In[8]:
 
 
+# 参数：layer是当前ansatz层数
+# 功能：利用有限差分法计算梯度，存储在列表gradient内
+# 返回：列表gradient以及在ansatz_data下的函数值function_value
+
+def calculate_gradient(n,layer,ansatz_data):
+    print('ansatz_data = {}'.format(ansatz_data))
+    
+    # 梯度信息存储到列表gradient中
+    gradient = []
+    
+    # 获取当前ansatz_data下的代价函数值
+    result_K,result_P,result_I,function_value = cost_function(n,layer,ansatz_data)
+    
+    
+    # 输出当前ansatz_data下的能量值
+    print('result_K = {}, result_P = {},result_I = {},function_value = {}'.format(result_K,result_P,result_I,function_value))   
+    print('\n')
+    
+    m = len(ansatz_data)
+    # 有限差分法,这个delta可以看作是一个超参数，太小，算出的梯度为0，太大，有限差分法算出的梯度和实际梯度之间的误差会很大，会报错
+    # 所以跑代码的过程中要结合比特数n的大小来调整这个delta，
+    delta = 1e-6
+    
+    for i in range(0,m):
+        data = ansatz_data.copy()
+        data[i] = data[i] + delta
+        
+        # 获取代价函数值
+        result_K,result_P,result_I,function_value1 = cost_function(n,layer,data)
+        
+        gap = function_value1 - function_value
+        gradient.append(gap/delta)
+    print('gradient = {}'.format(gradient))
+    print('\n')
+        
+    # 以列表形式返回梯度信息
+    return function_value, gradient   
+
+
+# In[9]:
+
+
 # 计算当前参数对应的量子态phi_lamada对应的函数值
 def cost_function(n,layer,ansatz_data):
     
     # 列表转为数组，一维数组
     ansatz_data = np.array(ansatz_data).astype(np.float32)
-    print('ansatz_data = {}'.format(ansatz_data))
+
    
     # 获取测量结果,info = {'十进制k':phi_k}
     info = get_info_state(n,layer,ansatz_data)
@@ -199,29 +244,61 @@ def cost_function(n,layer,ansatz_data):
     result_I = calculate_I(n,layer,ansatz_data,info)
     
     function_value = result_K + result_P + result_I
-    
-    # 输出当前ansatz_data下的能量值
-    print('result_K = {}, result_P = {},result_I = {},function_value = {}'.format(result_K,result_P,result_I,function_value))   
-    print('\n')
-    
-
-  
-    return function_value
-
-
-# In[9]:
-
-
-def get_expectation(n,layer,ansatz_data):
-    def execute_circ(ansatz_data):
-        return cost_function(n,layer,ansatz_data)
-    return execute_circ
+   
+    return result_K, result_P, result_I,function_value
 
 
 # In[10]:
 
 
-# 初始化参数以及训练
+def get_expectation(n,layer,ansatz_data):
+    def execute_circ(ansatz_data):
+        return calculate_gradient(n,layer,ansatz_data)
+    return execute_circ
+
+
+# In[11]:
+
+
+# 方式1：全局优化策略
+# 参数：量子比特数n以及ansatz总层数p
+# 功能：一次性搭建p层ansatz电路，并且实现参数随机初始化、调用scipy内的minimize函数进行参数优化， 
+# 返回：迭代停止时的参数ansatz_data
+def global_training(n,p):
+    # ansatz_data初始化
+    initial_ansatz = []
+    m  = len(ansatz.params_name)
+    for i in range(0,m):
+        parameter= random.random()
+        parameter = parameter*3.14
+        # 保留小数点之后五位，四舍五入
+        parameter = Decimal(parameter).quantize(Decimal("0.00001"), rounding = "ROUND_HALF_UP")
+        initial_ansatz.append(float(parameter))
+
+    # 类型转换,列表转换为一维数组
+    initial_ansatz = np.array(initial_ansatz).astype(np.float32)
+    print('\n')
+    print('p = {},initial_ansatz = {}'.format(p,initial_ansatz)) 
+    print('\n\n')
+
+
+    # 最小化代价函数
+    out = minimize(fun = get_expectation(n,p,initial_ansatz),x0 = initial_ansatz, method="BFGS",jac = True, options={'eps':1e-02,'return_all':True,'disp':True,'maxiter':1000}) 
+    print(out)
+
+    # 获取优化后的参数，存储到ansatz_data
+    ansatz_data = out['x']
+    
+    # 返回迭代停止时的参数信息
+    return ansatz_data
+
+
+# In[12]:
+
+
+# 参数：layer即ansatz的层数，counts为参数优化重复的次数（可见说明文档，里面有关于Fixing strategy策略实现的详细说明）
+# 功能：实现前layer-1层参数的赋值以及第layer层参数的随机初始化，利用scipy内minimize函数实现全体参数的优化
+# 返回：电路为layer层，代价函数收敛时对应的的参数ansatz_data以及function_value
 def train_parameters(layer,counts,ansatz_data):
    # 记录深度为layer时，优化后函数值和优化后参数之间的对应关系
     info = {}
@@ -231,7 +308,7 @@ def train_parameters(layer,counts,ansatz_data):
         # 电路层数为layer时，ansatz_data参数的初始化
         if layer == 1:
             initial_ansatz = []
-            for i in range(0,2*n):
+            for i in range(0,(layer + 1)*n):
                 parameter= random.random()
                 parameter = parameter*3.14
                 # 保留小数点之后五位，四舍五入
@@ -255,8 +332,7 @@ def train_parameters(layer,counts,ansatz_data):
         print('第{}次随机初始化,layer = {},initial_ansatz = {}'.format(times,layer,initial_ansatz)) 
 
         # 参数优化部分
-        out = minimize(fun = get_expectation(n,layer,initial_ansatz), x0 = initial_ansatz,tol=10e-8, method="COBYLA", options={'disp':True,'maxiter':500})
-#         out = minimize(fun = get_expectation(n,layer,encoder_data,initial_ansatz), x0 = initial_ansatz,tol=10e-8, method="Nelder-Mead", options={'disp':True,'maxiter':1000})
+        out = minimize(fun = get_expectation(n,layer,initial_ansatz),x0 = initial_ansatz, method="BFGS",jac = True, options={'eps':1e-02,'return_all':True,'disp':True,'maxiter':1000}) 
         print(out)
         
         # 记录优化后参数以及优化结果之间的对应关系，out['x']为优化后的参数，out['fun']为优化后的函数值
@@ -277,10 +353,12 @@ def train_parameters(layer,counts,ansatz_data):
     return ansatz_data,min_loss
 
 
-# In[11]:
+# In[13]:
 
 
-# 采用分层搭建策略，Fixing strategy
+# 方式2： Fixing strategy
+# 功能：逐层搭建ansatz电路，直至电路层数为p，同时实现参数优化
+# 返回：电路为p层时，代价函数收敛时对应的参数ansatz_data以及function_value
 def layerwise_training(n,p):  
     # 根据层数确定有counts次随机初始化参数以及counts次参数训练的过程，并将每次优化结果存储在loss中
     counts = 3
@@ -303,7 +381,7 @@ def layerwise_training(n,p):
     return data, min_loss
 
 
-# In[12]:
+# In[14]:
 
 
 n = int(input('请输入量子比特数个数：'))
@@ -323,7 +401,7 @@ s1 = 20000
 s2 = 100
 
 
-# In[13]:
+# In[15]:
 
 
 # 电路图可视化，n为量子比特数，p是ansatz层数
@@ -332,43 +410,7 @@ print('含参酉电路搭建如下：')
 ansatz.svg()
 
 
-# In[14]:
-
-
-# 全局优化策略
-def global_training(n,p):
-    # ansatz_data初始化
-    initial_ansatz = []
-    m  = len(ansatz.params_name)
-    for i in range(0,m):
-        parameter= random.random()
-        parameter = parameter*3.14
-        # 保留小数点之后五位，四舍五入
-        parameter = Decimal(parameter).quantize(Decimal("0.00001"), rounding = "ROUND_HALF_UP")
-        initial_ansatz.append(float(parameter))
-
-    # 类型转换,列表转换为一维数组
-    initial_ansatz = np.array(initial_ansatz).astype(np.float32)
-    print('\n')
-    print('p = {},initial_ansatz = {}'.format(p,initial_ansatz)) 
-    print('\n\n')
-
-
-    # 最小化代价函数
-    # out = minimize(fun = get_expectation(n,p,initial_ansatz), x0 = initial_ansatz, method="COBYLA", options={'disp':True,'maxiter':1000})
-
-    out = minimize(fun = get_expectation(n,p,initial_ansatz), x0 = initial_ansatz, method="BFGS",options={'eps':1e-06,'disp':True,'maxiter':400}) 
-    print(out)
-
-    # 获取优化后的参数，存储到ansatz_data
-    ansatz_data = out['x']
-    
-    # 返回迭代停止时的参数信息
-    return ansatz_data
-    
-
-
-# In[15]:
+# In[16]:
 
 
 # 获取f(x_k)*f(x_k)和x_k的数值变化关系
@@ -380,18 +422,21 @@ for i in range(0,2**n):
 
 # 获取优化后的参数信息
 
-# Fixing strategy
-ansatz_data,min_loss = layerwise_training(n,p)
+# 策略1：全局
+ansatz_data = global_training(n,p)
+
+# 策略2：Fixing strategy,策略2只是提供了另一种思路，我们希望它可以获得更好的数值结果，但是通过实际优化效果来看和重复counts次策略1效果相差不大
+# 感兴趣的可以看一下这部分，不感兴趣的自行绕过即可
+# ansatz_data,min_loss = layerwise_training(n,p)
 print(ansatz_data,min_loss)
 
-# 全局
-# ansatz_data = global_training(n,p)
+
 ansatz_data = np.array(ansatz_data).astype(np.float32)
 print('优化得到的ansatz_data = {}'.format(ansatz_data))
 print('\n')
 
 
-# In[ ]:
+# In[17]:
 
 
 # 根据优化后的参数信息，获取info = {'k':phi_k}，k为十进制数
@@ -412,6 +457,8 @@ for k,value in info.items():
     f.append(value**2)
     
 # 绘制x_k和f(x_k)平方的数值关系
+# 生成柱状图
+# plt.bar(x,f,0.5,color="blue")
 plt.plot(x,f)
 plt.xlabel('x_k')  # x轴标题
 plt.ylabel('f(x_k)*f(x_k)')  # y轴标题
@@ -430,7 +477,7 @@ print('\n')
 # 获取ansatz_data输出，合并到encoder_data数据中
 pr1 = dict(zip(ansatz.params_name, ansatz_data)) # 获取线路参数
 
-# 向字典pr中添加参数信息，获取encoder以及ansatz_data的完整信息
+# 向字典pr中添加参数信息，获取ansatz_data的完整信息
 pr = {}
 for key,value in pr1.items():
     pr[key] = value
