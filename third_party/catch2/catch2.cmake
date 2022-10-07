be526f835085285bb656e8eeebf3e3d7c887bd0f
Damien Nguyen <damien1@huawei.com>
2022-10-07 10:10:17 +0200

    [CMAKE] Add patch for Catch2 to fix strnlen calls on some platforms

--- third_party/catch2/catch2.cmake
+++ third_party/catch2/catch2.cmake
@@ -40,12 +40,18 @@ set(CMAKE_OPTION
     -DCMAKE_CXX_EXTENSIONS=${CMAKE_CXX_EXTENSIONS}
     -DCMAKE_POSITION_INDEPENDENT_CODE=ON)
 
+set(PATCHES)
+if(CYGWIN OR OS_NAME STREQUAL "MSYS-MSYS")
+  list(APPEND PATCHES ${CMAKE_CURRENT_LIST_DIR}/patch/windows-fix-strnlen.patch001)
+endif()
+
 # cmake-lint: disable=E1122
 mindquantum_add_pkg(
   Catch2
   VER ${VER}
   URL ${REQ_URL}
   MD5 ${MD5}
+  PATCHES ${PATCHES}
   CMAKE_PKG_NO_COMPONENTS
   CMAKE_OPTION ${CMAKE_OPTION}
   TARGET_ALIAS mindquantum::catch2 Catch2::Catch2
