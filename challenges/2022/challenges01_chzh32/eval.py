c92b7a350890f403fb7de3e9e2ad51f4b37f1202
pumpkin <chzh32@163.com>
2022-10-10 04:03:37 +0000

    难题挑战赛第一期作品提交
    Signed-off-by: pumpkin <chzh32@163.com>

---
+++ challenges/2022/challenges01_chzh32/eval.py
@@ -0,0 +1,65 @@
+import sys
+sys.path.append("./src")
+import os
+os.environ['OMP_NUM_THREADS'] = '4'
+from src.main import Main, Timer
+import mindquantum
+import numpy as np
+
+# molecule names: H6, LiH, BeH2, NH3, CH4
+molecules = ['H6_0.8', 'H6_0.9', 'H6_1.0', #H6
+           'LiH_0.4', 'LiH_0.8', 'LiH_1.2', 'LiH_1.6', 'LiH_2.0', 'LiH_2.4', 'LiH_2.8', 'LiH_3.2', 'LiH_3.6', 'LiH_4.0', #LiH
+           'BeH2_1.4', 'BeH2_1.5', 'BeH2_1.6', #BeH2
+           'NH3_1.0', 'NH3_1.2', 'NH3_1.4' #NH3
+            ]
+prefixes = ['H6','H6','H6',
+            'LiH','LiH','LiH','LiH','LiH','LiH','LiH','LiH','LiH','LiH',
+            'BeH2','BeH2','BeH2',
+            'NH3','NH3','NH3'
+            ]
+fci_en = [-3.2044118794841037, -3.2445422400448987, -3.2360662798923423,
+          -6.6402778359670505, -7.63416732932434, -7.852430853195922, -7.882324378883506, -7.861087772481495, -7.8306316244241145, -7.806763402564964, -7.793274300660556, -7.786991815386028, -7.784278178715392,
+          -15.59074334548809, -15.57605124523192, -15.554623690074516,
+          -55.519219000876646, -55.48265755893588, -55.37483622141084
+            ]
+baseline = [16.0, 16.0, 16.0,
+            5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 
+            8.5, 8.5, 8.5,
+            155., 155., 155.
+            ]
+err_num = np.ones(len(molecules))
+
+if __name__ == "__main__":
+    with open('./output_info.o', 'a') as f:
+        main = Main()
+        timer = Timer()
+        en_list, time, s = [], [], []
+        for idx in range(len(molecules)):
+            t0 = timer.runtime()
+            print('Start: ', molecules[idx], file=f)
+            mol_file = './src/hdf5files/'+molecules[idx]+'.hdf5'
+            en_list.append(main.run(prefixes[idx], mol_file))
+            if (abs(en_list[-1] - fci_en[idx]) <= 0.0016): 
+                err_num[idx] = 0
+                time.append(timer.runtime()- t0)
+            else:
+                time.append(3600)
+            s.append(time[idx]/baseline[idx])
+
+        if len(en_list) != len(fci_en):
+                print('The length of en_list is not equal to that of fci_en !', file=f)
+
+        score = sum(s)
+   
+        print('Time: ', time, file=f)
+        print('Num_err: ', err_num, file=f)
+        print('Molecular_names: ', molecules, file=f)
+        #print(prefixes, file=f)
+        print('FCI_energies: ', fci_en, file=f)
+        print('Result_energies: ', en_list, file=f)
+        print('Baseline_time: ', baseline, file=f)
+        print('Case_scores: ', s, file=f)
+        print('Total_Score: ', score, file=f)
+
+        print('Acc: ', score)
+    
---
+++ challenges/2022/challenges01_chzh32/src/main.py
@@ -0,0 +1,131 @@
+import os
+os.environ['OMP_NUM_THREADS'] = '4'
+import sys
+#from hiqfermion.drivers import MolecularData
+from openfermion import MolecularData
+from openfermionpyscf import run_pyscf
+import time
+import numpy as np
+from mindquantum import Simulator, Hamiltonian, X, Circuit
+from scipy.optimize import minimize
+#from mindquantum.algorithm import generate_uccsd
+import matplotlib.pyplot as plt
+from uccsd_generator import Uccsd_Generator
+
+class Timer:
+    def __init__(self, t0=0.0):
+        self.start_time = time.time()
+        self.t0 = t0
+
+    def runtime(self):
+        return time.time() - self.start_time + self.t0
+
+    def resetime(self):
+        self.start_time = time.time()
+
+def format_time(t):
+    hh = t // 3600
+    mm = (t - 3600 * hh) // 60
+    ss = t % 60
+    return hh, mm, ss
+
+
+def func(x, grad_ops, show_iter_val=False):
+    f, g = grad_ops(x)
+    if show_iter_val:
+        print(np.real(np.squeeze(f)))
+    return np.real(np.squeeze(f)), np.squeeze(g)
+
+def param2dict(keys, values):
+    param_dict = {}
+    for (key, value) in zip(keys, values):
+        param_dict[key] = value
+    return param_dict
+
+def load_molecular_file(molecular_file):
+    molecule = MolecularData(filename=molecular_file, data_directory='./src/hdf5files/')
+    molecule = MolecularData(geometry=molecule.geometry, 
+                            basis=molecule.basis, 
+                            multiplicity=molecule.multiplicity,  
+                            charge=molecule.charge, 
+                            filename=molecular_file,
+                            data_directory='./src/hdf5files/')
+    return molecule
+
+class VQEoptimizer:
+    def __init__(self, molecule=None, amp_th=0, seed=1202, file=None):
+        #self.timer = Timer()
+        self.molecule = molecule
+        self.amp_th = amp_th
+        self.backend = 'projectq'
+        self.seed = seed
+        self.file = file
+        self.init_amp = []
+
+        if molecule != None:
+            self.generate_circuit(molecule)
+
+    def generate_circuit(self, molecule=None, seed=1202):
+        if molecule == None:
+            molecule = self.molecule
+        self.circuit = Circuit([X.on(i) for i in range(molecule.n_electrons)])
+
+        ansatz_circuit, \
+        self.init_amp, \
+        self.params_name, \
+        self.hamiltonian, \
+        self.n_qubits, \
+        self.n_electrons = Uccsd_Generator(molecule, self.amp_th, 'JW').generate_uccsd
+
+        self.circuit += ansatz_circuit 
+        self.simulator = Simulator(self.backend, self.n_qubits, seed)
+
+    def optimize(self, operator=None, circuit=None, init_amp=[],
+                 method='bfgs', maxstep=200, iter_info=False, tol=1e-5):
+        if operator == None:
+            operator = self.hamiltonian
+        if circuit == None:
+            circuit = self.circuit
+        if np.array(init_amp).size == 0:
+            init_amp = self.init_amp
+
+        grad_ops = self.simulator.get_expectation_with_grad(Hamiltonian(operator), circuit)
+        self.res = minimize(func, init_amp,
+                            args=(grad_ops, iter_info),
+                            method=method,
+                            jac=True, 
+                            options={'gtol': tol}
+                            )
+
+class Main:
+    def __init__(self):
+        super().__init__()
+        self.work_dir = './src/'
+
+    '''    
+    def run(self, prefix, molecular_file):
+        try:
+            return self._run(prefix=prefix, molecular_file=molecular_file)
+        except Exception as e:
+            print(f'{prefix} throw an error: ', e)
+            return 0
+    '''
+    def run(self, prefix, molecular_file):
+        ath = 0.005
+        #if 'lih' in prefix.lower() or 'lih' in molecular_file.lower():
+        #    ath = 0.005
+        if 'ch4' in prefix.lower() or 'ch4' in molecular_file.lower():
+            ath = 0.001
+
+        molecule = load_molecular_file(molecular_file)
+        #molecule.load()
+
+        vqe = VQEoptimizer(amp_th=ath)
+
+        mol = run_pyscf(molecule, run_scf=1, run_ccsd=1, run_fci=0)
+        vqe.generate_circuit(mol)
+
+        vqe.optimize(tol=1e-2)
+        en = vqe.res.fun
+
+        return en
