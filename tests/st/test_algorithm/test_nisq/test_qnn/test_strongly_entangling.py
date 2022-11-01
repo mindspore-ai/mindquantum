af88583a1cfa3be7eb8d1eef03e50ffa021fcfe2
donghufeng <xuxusheng2@huawei.com>
2022-11-02 07:52:07 +0800

    change test assert for strongly entangling

--- tests/st/test_algorithm/test_nisq/test_qnn/test_strongly_entangling.py
+++ tests/st/test_algorithm/test_nisq/test_qnn/test_strongly_entangling.py
@@ -14,6 +14,8 @@
 # ============================================================================
 """Test strongly entangling"""
 
+import numpy as np
+
 from mindquantum.algorithm.nisq import StronglyEntangling
 from mindquantum.core.gates import X
 
@@ -28,4 +30,8 @@ def test_strongly_entangling_ansatz():
     assert len(circ) == 12
     assert len(circ.params_name) == 18
     assert circ[-1] == X.on(1, 2)
-    assert len(circ.__str__()) == 1541
+    qs_exp = np.array([
+        0.79707728 + 0.01013428j, -0.11230256 + 0.0640159j, -0.02478449 - 0.09007194j, 0.19063718 + 0.21396115j,
+        0.05847713 + 0.11265412j, -0.00747775 + 0.16495718j, 0.25717824 + 0.29613843j, 0.22900849 + 0.08570448j
+    ])
+    assert np.allclose(qs_exp, circ.get_qs(pr=np.linspace(0, 1, 18)))
