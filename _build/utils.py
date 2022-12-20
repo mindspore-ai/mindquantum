6b83b425f92a4f17b1a7d923f22165b212deea96
Damien Nguyen <ngn.damien@gmail.com>
2022-12-20 09:06:36 +0000

    !1389 Fix license header for Python setup code from ProjectQ * [PY] Add missing license information from ProjectQ

--- _build/utils.py
+++ _build/utils.py
@@ -1,4 +1,5 @@
 #   Copyright 2022 <Huawei Technologies Co., Ltd>
+#   Copyright 2017 ProjectQ-Framework (www.projectq.ch)
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
