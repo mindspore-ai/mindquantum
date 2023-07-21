73f0729e73f2525f5f6a127662360992f87dc51f
dsdsdshe <jedzhou99@163.com>
2023-07-21 15:20:56 +0800

    clean up research

--- meeting/chengdu_meetup/copy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "copy qaoa.ipynb to qaoa_$1.ipynb"
-cp qaoa.ipynb qaoa_$1.ipynb
-echo "copy Learning_MindQuantum.ipynb to Learning_MindQuantum_$1.ipynb"
-cp Learning_MindQuantum.ipynb Learning_MindQuantum_$1.ipynb
--- meeting/ustc/copy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "copy do_it_yourself1.ipynb to do_it_yourself1_$1.ipynb"
-cp do_it_yourself1.ipynb do_it_yourself1_$1.ipynb
-echo "copy do_it_yourself2.ipynb to do_it_yourself2_$1.ipynb"
-cp do_it_yourself2.ipynb do_it_yourself2_$1.ipynb
