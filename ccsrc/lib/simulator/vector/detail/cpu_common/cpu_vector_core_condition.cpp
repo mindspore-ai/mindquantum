da25f1109d803a0434dc9926357d1b4890b88ac1
xuxusheng <xuxusheng2@huawei.com>
2023-07-26 15:36:57 +0800

    add missing header for msvc

--- ccsrc/lib/simulator/vector/detail/cpu_common/cpu_vector_core_condition.cpp
+++ ccsrc/lib/simulator/vector/detail/cpu_common/cpu_vector_core_condition.cpp
@@ -11,6 +11,8 @@
 //   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
+#include <functional>
+
 #include "config/openmp.hpp"
 
 #include "math/pr/parameter_resolver.hpp"
