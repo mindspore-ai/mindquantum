0994128bbbd476d0db4cd80ecb80f19cdca06628
Waikikilick <runhong93@qq.com>
2023-09-28 09:47:48 +0000

    ospp_project_from_herunhong
    Signed-off-by: Waikikilick <runhong93@qq.com>

---
+++ summer_ospp/2023/2349a0462/psho_direct.py
@@ -0,0 +1,118 @@
+from openfermion.chem import MolecularData
+from openfermionpyscf import run_pyscf
+from mindquantum import *
+from mindquantum.algorithm.nisq import generate_uccsd
+
+# PSHO 直接法
+# 使用 openfermion 生成分子数据，可以在本地 Linux 环境下运行
+# 在构造量子线路和求期望值时，都去掉了哈密顿量中的常数项
+# 以 H2 分子为例
+# 时间演化因子采用二阶 Trotter 近似
+# 将辅助比特放在了最后一位
+
+
+# 定义分子结构，以 H2 为例
+blen = 0.74 # 键长
+geom = [["H", [0.0, 0.0, 0.0 * blen]],
+        ["H", [0.0, 0.0, 1.0 * blen]],]
+basis = "sto3g" # 基底
+spin = 0
+
+# 获取分子数据
+molecule_data = MolecularData(geom, basis, multiplicity=2 * spin + 1)
+molecule_of = run_pyscf(molecule_data, run_scf=0, run_ccsd=1, run_fci=1) 
+fci_res = molecule_of.fci_energy # fci 计算结果
+print('fci 结果为：', fci_res) 
+
+_,_,_,ham_mol, mol_n_qubits, n_electrons = generate_uccsd(molecule_data, threshold=0)
+# ham_mol 是一个 QubitOperator
+# 形如： -0.0971 [] + 0.1714 [Z0] + 0.1714 [Z1] + ...
+
+ham_list0 = [] # 用于承接分子哈密顿算符的每一个子项（除常数项外）。
+# 形如： [0.1714*delta [Z0], 0.1714*delta [Z1], -0.2234*delta [Z2], ...]
+
+for i, term in enumerate(ham_mol.terms): # 去掉第一项常数项，该项不用于构造线路。
+    if i ==0:
+        constant =  ham_mol.get_coeff(term).const.real
+        print('常数项系数为：', constant)
+        constant_term = term
+    else:
+        ham_list0.append(QubitOperator(term, ham_mol.get_coeff(term).const) *  ParameterResolver('delta') )  # 给每一项都加上同一个系数。后面用于构造含参时间演化算符线路。
+
+ham_list1 = [] # 操作 ham_list0, 后面作为对时间演化算符进行 trotter 分解时依次执行的字符串
+# 形如：[0.1714*t [Z0], 0.1714*t [Z1], ..., 0.1206*t [Z1 Z3], 0.1744*t [Z2 Z3], 0.1206*t [Z1 Z3], ...,    0.1714*t [Z1], 0.1714*t [Z0] ]
+
+for term in ham_list0: # trotter 近似的前半部分
+    ham_list1.append(term)
+
+for i in range(len(ham_list0)-2, -1, -1): # trotter 近似的后半部分
+    ham_list1.append(ham_list0[i])
+
+ham_H = Hamiltonian(ham_mol - QubitOperator(constant_term, constant) ) # 论文中 H 的期望值（去掉了常数项）
+# 形如：  0.1714 [Z0] + 0.1714 [Z1] + ...
+
+
+tau = 1.4 # 演化时间 tau = (np.pi/2) / E_0
+delta = 0.01
+num_slices = int(tau/delta)
+
+# 算符 e^{i*H*tau} 
+circ1 = Circuit()
+for i, term in enumerate(ham_list1):
+    if i==int(len(ham_list1)/2):
+        circ1 += TimeEvolution(term.subs({'delta':-delta})).circuit
+    else:
+        circ1 += TimeEvolution(term.subs({'delta':-delta/2})).circuit
+circ1 *= num_slices
+
+# 算符 e^{-i*H*tau}
+circ2 = Circuit()
+for i, term in enumerate(ham_list1):
+    if i==int(len(ham_list1)/2):
+        circ2 += TimeEvolution(term.subs({'delta':delta})).circuit
+    else:
+        circ2 += TimeEvolution(term.subs({'delta':delta/2})).circuit
+circ2 *= num_slices
+
+hatree = Circuit([X.on(i) for i in range(n_electrons)])   # 生成 ref 态态的期望值（分子）
+
+def sigma():
+    circ = Circuit()
+
+    circ += H.on(4) # 辅助比特在最下面
+    circ += S.on(4)
+    circ += H.on(4)
+
+    circ += controlled(circ1)(4) # 时间演化算符线路受辅助比特控制
+    circ += X.on(4)
+    circ += controlled(circ2)(4)
+    circ += X.on(4)
+
+    circ += H.on(4)
+    circ += S.on(4)
+    circ += H.on(4)
+    circ += Measure().on(4)
+    return circ
+
+
+sim = Simulator('mqvector', 5)
+sim.apply_circuit(hatree)
+state = sim.get_qs()
+
+error = []
+j = 0 # 辅助比特连续测试为 0 的次数
+for i in range(25):
+    sim.apply_circuit(sigma())
+    state_str = sim.get_qs(True)
+    state_tem = sim.get_qs()
+    if state_str[-6] == '0': # 如果辅助比特测量结果为 0 就继续运行下一轮，否则无视上一轮线路的操作，重新来
+        j += 1
+        state = state_tem
+        res = sim.get_expectation(ham_H)
+        print(j, abs(fci_res - (res + constant)))
+        error.append(abs(fci_res - (res + constant)))
+    else:
+        sim.set_qs(state)
+
+print('运行完啦!')
+print(error)
---
+++ summer_ospp/2023/2349a0462/psho_noise.py
@@ -0,0 +1,141 @@
+from openfermion.chem import MolecularData
+from openfermionpyscf import run_pyscf
+from mindquantum import *
+from mindquantum.algorithm.nisq import generate_uccsd
+
+# PSHO 间接法
+# 使用 openfermion 生成分子数据，可以在本地 Linux 环境下运行
+# 在构造量子线路和求期望值时，都去掉了哈密顿量中的常数项
+# 以 H2 分子为例
+# 时间演化因子采用二阶 Trotter 近似
+# 将辅助比特放在了最后一位
+
+
+# 定义分子结构，以 H2 为例
+blen = 0.74 # 键长
+geom = [["H", [0.0, 0.0, 0.0 * blen]],
+        ["H", [0.0, 0.0, 1.0 * blen]],]
+basis = "sto3g" # 基底
+spin = 0
+
+# 获取分子数据
+molecule_data = MolecularData(geom, basis, multiplicity=2 * spin + 1)
+molecule_of = run_pyscf(molecule_data, run_scf=0, run_ccsd=1, run_fci=1) 
+fci_res = molecule_of.fci_energy # fci 计算结果
+print('fci 结果为：', fci_res) 
+
+_,_,_,ham_mol, mol_n_qubits, n_electrons = generate_uccsd(molecule_data, threshold=0)
+# ham_mol 是一个 QubitOperator
+# 形如： -0.0971 [] + 0.1714 [Z0] + 0.1714 [Z1] + ...
+
+ham_list0 = [] # 用于承接分子哈密顿算符的每一个子项（除常数项外）。
+# 形如： [0.1714*delta [Z0], 0.1714*delta [Z1], -0.2234*delta [Z2], ...]
+
+for i, term in enumerate(ham_mol.terms): # 去掉第一项常数项，该项不用于构造线路。
+    if i ==0:
+        constant =  ham_mol.get_coeff(term).const.real
+        print('常数项系数为：', constant)
+        constant_term = term
+    else:
+        ham_list0.append(QubitOperator(term, ham_mol.get_coeff(term).const) *  ParameterResolver('delta') )  # 给每一项都加上同一个系数。后面用于构造含参时间演化算符线路。
+
+ham_list1 = [] # 操作 ham_list0, 后面作为对时间演化算符进行 trotter 分解时依次执行的字符串
+# 形如：[0.1714*t [Z0], 0.1714*t [Z1], ..., 0.1206*t [Z1 Z3], 0.1744*t [Z2 Z3], 0.1206*t [Z1 Z3], ...,    0.1714*t [Z1], 0.1714*t [Z0] ]
+
+for term in ham_list0: # trotter 近似的前半部分
+    ham_list1.append(term)
+
+for i in range(len(ham_list0)-2, -1, -1): # trotter 近似的后半部分
+    ham_list1.append(ham_list0[i])
+
+ham_H = Hamiltonian(ham_mol - QubitOperator(constant_term, constant) ) # 论文中 H 的期望值（去掉了常数项）
+# 形如：  0.1714 [Z0] + 0.1714 [Z1] + ...
+
+ham_HZ = Hamiltonian((ham_mol - QubitOperator(constant_term, constant)) * QubitOperator(f'Z{mol_n_qubits}', -1)) # 论文中 -ZH 的期望值 （去掉了常数项）
+# 形如： -0.1714 [Z0 Z4] + -0.1714 [Z1 Z4] + ...
+
+ham_IZ = Hamiltonian(QubitOperator(f'Z{mol_n_qubits}', -1))                # 论文中 -ZI 的期望值
+# 形如： -1 [Z4] 
+
+def com(n,m): # 用于求组合数 C_nm，n >= m
+    res0 = 1
+    for i in range(n-m+1, n+1, 1):
+        res0 *= i
+    res1 = 1
+    for i in range(1,m+1):
+        res1 *= i
+    return res0/res1
+
+hatree = Circuit([X.on(i) for i in range(n_electrons)])   # 生成 ref 态
+hatree += I.on(mol_n_qubits-1) # 加上一个单位门，用于扩展量子线路的宽度，使其与时间演化分子的比特数对齐，从而方便在后面求 ref 态的期望值（分子）
+
+
+tau = 1.4 # 演化时间 tau = (np.pi/2) / E_0
+delta = 0.1
+for amp in [0.00001, 0.0001, 0.001, 0.01, 0.1]: # 噪声幅值
+    n = 10
+    ### 求解分子和分母的第一项：
+    An1 = 0
+    Bn1 = 0
+
+    for k in range(n):
+        t = tau*(n-k)
+        num_slices = int(t/delta) # trotter 分解级数
+
+        # 算符 e^{i*H*t} 
+        circ1 = Circuit()
+        for i, term in enumerate(ham_list1):
+            if i==int(len(ham_list1)/2):
+                circ1 += TimeEvolution(term.subs({'delta':-delta})).circuit
+            else:
+                circ1 += TimeEvolution(term.subs({'delta':-delta/2})).circuit
+        circ1 *= num_slices
+
+        # 算符 e^{-i*H*t}
+        circ2 = Circuit()
+        for i, term in enumerate(ham_list1):
+            if i==int(len(ham_list1)/2):
+                circ2 += TimeEvolution(term.subs({'delta':delta})).circuit
+            else:
+                circ2 += TimeEvolution(term.subs({'delta':delta/2})).circuit
+        circ2 *= num_slices
+
+        circ = Circuit()
+        circ += hatree
+
+        circ += H.on(circ1.n_qubits) # 辅助比特在最下面
+        circ += S.on(circ1.n_qubits)
+        circ += H.on(circ1.n_qubits)
+
+        circ += controlled(circ1)(circ1.n_qubits) # 时间演化算符线路受辅助比特控制
+        circ += X.on(circ1.n_qubits)
+        circ += controlled(circ2)(circ1.n_qubits)
+        circ += X.on(circ1.n_qubits)
+
+        circ += H.on(circ1.n_qubits)
+        circ += S.on(circ1.n_qubits)
+        circ += H.on(circ1.n_qubits)
+        circ = circ.with_noise(AmplitudeDampingChannel(amp), also_ctrl=True)
+        
+        sim1 = Simulator('mqvector', circ.n_qubits)
+        sim1.apply_circuit(circ)
+
+        value1 = sim1.get_expectation(ham_HZ).real # 论文中 -ZH 的期望值
+        value2 = sim1.get_expectation(ham_IZ).real   # 论文中 -ZI 的期望值
+
+        An1 += (-1)**k*2*com(2*n, k)*value1
+        Bn1 += (-1)**k*2*com(2*n, k)*value2
+
+    ### 求分子和分母的第二项
+
+    sim2 = Simulator('mqvector', hatree.n_qubits)
+    sim2.apply_circuit(hatree.with_noise(AmplitudeDampingChannel(amp), also_ctrl=True))
+
+    An2 = com(2*n, n)*(-1)**n*(sim2.get_expectation(ham_H).real)
+    Bn2 = com(2*n, n)*(-1)**n
+
+    res =  (An1+An2)/(Bn1+Bn2)
+
+    print('噪声强度为：', amp)
+
+    print('与 fci 的差值为：', abs(fci_res - (res + constant)))
