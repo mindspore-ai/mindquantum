f7d8aa658a513db1ffb25a2cfbdd8ec24aed7dda
左博伟 <12331495@mail.sustech.edu.cn>
2025-04-21 08:51:34 +0000

    add example.
    Signed-off-by: 左博伟 <12331495@mail.sustech.edu.cn>

---
+++ example/approximation of single-qubit unitaries/utils/state.py
@@ -0,0 +1,270 @@
+"""class State wraps a tensor as underlying representation."""
+
+import cmath
+import math
+import random
+import sys
+from typing import List, Tuple
+
+import numpy as np
+from src import helper
+from src import tensor
+
+
+class State(tensor.Tensor):
+  """class State represents single and multi-qubit states."""
+
+  def density(self) -> tensor.Tensor:
+    return tensor.Tensor(np.outer(self, self.conj()))
+
+  def adjoint(self):
+    return self.conj().transpose()
+
+  def normalize(self):
+    """Renormalize the state. Sum of squared amplitudes==1.0."""
+
+    dprod = np.conj(self) @ self
+    assert not dprod.is_close(0.0), 'Normalizing to 0-probability state'
+    self /= np.sqrt(np.real(dprod))  # modify object in place.
+    return self
+
+  def ampl(self, *bits: Tuple[int]) -> np.complexfloating:
+    """Return amplitude for state indexed by 'bits'."""
+
+    return self[helper.bits2val(bits)]
+
+  def prob(self, *bits: Tuple[int, ...]) -> float:
+    """Return probability for state indexed by 'bits'."""
+
+    amplitude = self.ampl(*bits)
+    return np.real(amplitude.conj() * amplitude)
+
+  def phase(self, *bits: Tuple[int, ...]) -> float:
+    """Return phase of a state from the complex amplitude."""
+
+    amplitude = self.ampl(*bits)
+    return math.degrees(cmath.phase(amplitude))
+
+  def diff(self, psi, dump=True) -> bool:
+    """Print element-wise differences to another state."""
+
+    same = True
+    for idx, val in enumerate(self):
+      if not cmath.isclose(val, psi[idx], abs_tol=1e-4):
+        same = False
+        if dump:
+          print(f'State{helper.val2bits(idx, self.nbits)} (|{idx}>):', end='')
+          print(f'{val:+.3f}  {psi[idx]:+.3f}')
+    return same
+
+  def maxprob(self) -> Tuple[List[float], float]:
+    """Find state with highest probability."""
+
+    # This is the as described in the book, which is good
+    # for learning:
+    # maxbits, maxprob = [], 0.0
+    # for bits in helper.bitprod(self.nbits):
+    #   cur_prob = self.prob(*bits)
+    #   if cur_prob > maxprob:
+    #     maxbits, maxprob = bits, cur_prob
+
+    # However, we can do a lot faster. We just iterate
+    # over the state vector, find the index of the maximum
+    # amplitude with a numpy function, and return the probability
+    # and the index as a bitstring.
+    idx = np.abs(self).argmax()
+    maxprob = np.real(self[idx].conj() * self[idx])
+    maxbits = helper.val2bits(idx, self.nbits)
+    return maxbits, maxprob
+
+  def apply1(self, gate: np.ndarray, index: int) -> None:
+    """Apply single-qubit gate to this state."""
+
+    # To maintain qubit ordering in this infrastructure,
+    # index needs to be reversed.
+    index = self.nbits - index - 1
+    if index < 0:
+      print('***Error***: Negative qubit index in apply1().')
+      print('             Perhaps using wrongly shaped state?\n')
+      sys.exit(1)
+    pow_2_index = 1 << index
+    g00 = gate[0, 0]
+    g01 = gate[0, 1]
+    g10 = gate[1, 0]
+    g11 = gate[1, 1]
+    for g in range(0, 1 << self.nbits, 1 << (index + 1)):
+      for i in range(g, g + pow_2_index):
+        t1 = g00 * self[i] + g01 * self[i + pow_2_index]
+        t2 = g10 * self[i] + g11 * self[i + pow_2_index]
+        self[i] = t1
+        self[i + pow_2_index] = t2
+
+  def applyc(self, gate: np.ndarray, control: int, target: int) -> None:
+    """Apply a controlled 2-qubit gate via explicit indexing."""
+
+    # To maintain qubit ordering in this infrastructure,
+    # index needs to be reversed.
+    index = self.nbits - target - 1
+    if index < 0:
+      print('***Error***: Negative qubit index in applyc().')
+      print('             Perhaps using wrongly shaped state?\n')
+      sys.exit(1)
+    control = self.nbits - control - 1
+    pow_2_index = 1 << index
+    g00 = gate[0, 0]
+    g01 = gate[0, 1]
+    g10 = gate[1, 0]
+    g11 = gate[1, 1]
+    for g in range(0, 1 << self.nbits, 1 << (index + 1)):
+      idx_base = g * (1 << self.nbits)
+      for i in range(g, g + pow_2_index):
+        if (idx_base + i) & (1 << control):
+          t1 = g00 * self[i] + g01 * self[i + pow_2_index]
+          t2 = g10 * self[i] + g11 * self[i + pow_2_index]
+          self[i] = t1
+          self[i + pow_2_index] = t2
+
+  def dump(self, desc: str = None, prob_only: bool = True) -> None:
+    """Dump probabilities for a state, including local qubit state."""
+
+    def state_to_string(bits: Tuple[int]) -> str:
+      s = ''.join(str(i) for i in bits)
+      dec_digits = int(math.log10(2 ** len(bits))) + 1
+      return f'|{s}> (|{int(s, 2):{dec_digits}d}>)'
+
+    if desc:
+      print('|', end='')
+      for i in range(self.nbits):
+        print(i % 10, end='')
+      print(f"> '{desc}'")
+
+    state_list: List[str] = []
+    for bits in helper.bitprod(self.nbits):
+      if prob_only and (self.prob(*bits) < 10e-6):
+        continue
+      state_list.append(
+          '{:s}:  ampl: {:+.2f} prob: {:.2f} Phase: {:5.1f}'.format(
+              state_to_string(bits),
+              self.ampl(*bits),
+              self.prob(*bits),
+              self.phase(*bits),
+          )
+      )
+    state_list.sort()
+    print(*state_list, sep='\n')
+
+
+def qubit(alpha: complex = None, beta: complex = None) -> State:
+  """Produce a given state for a single qubit."""
+
+  if alpha is None and beta is None:
+    raise ValueError('alpha, beta, or both, need to be specified')
+
+  # Note that multiplying a complex conjugate with its non-conjugate
+  # is a real number, but we still have to type-cast it to avoid
+  # Python warnings (hence the use of np.real()).
+  if beta is None:
+    beta = np.sqrt(1.0 - np.real(np.conj(alpha) * alpha))
+  if alpha is None:
+    alpha = np.sqrt(1.0 - np.real(np.conj(beta) * beta))
+
+  if not math.isclose(
+      np.real(np.conj(alpha) * alpha) + np.real(np.conj(beta) * beta), 1.0
+  ):
+    raise ValueError('Qubit probabilities do not sum to 1.')
+
+  return State([alpha, beta])
+
+
+# The functions zeros() and ones() produce the all-zero or all-one
+# computational basis vector for `d` qubits, ie,
+#     |000...0> or
+#     |111...1>
+#
+# The result of this tensor product is
+#   always [1, 0, 0, ..., 0]^T or [0, 0, 0, ..., 1]^T
+
+
+def zeros_or_ones(d: int = 1, idx: int = 0) -> State:
+  """Produce the all-0/1 basis vector for `d` qubits."""
+
+  assert d > 0, 'Need to specify at least 1 qubit'
+  t = np.zeros(2**d, dtype=tensor.tensor_type())
+  t[idx] = 1
+  return State(t)
+
+
+def zeros(d: int = 1) -> State:
+  """Produce state with 'd' |0>, eg., |0000>."""
+
+  return zeros_or_ones(d, 0)
+
+
+def ones(d: int = 1) -> State:
+  """Produce state with 'd' |1>, eg., |1111>."""
+
+  return zeros_or_ones(d, 2**d - 1)
+
+
+def plus(d: int = 1) -> State:
+  """Product state |+>."""
+
+  return State([1 / np.sqrt(2), 1 / np.sqrt(2)]).kpow(d)
+
+
+def minus(d: int = 1) -> State:
+  """Product state |->."""
+
+  return State([1 / np.sqrt(2), -1 / np.sqrt(2)]).kpow(d)
+
+
+def plusi(d: int = 1) -> State:
+  """Product state |i>."""
+
+  return State([1 / np.sqrt(2), 1j / np.sqrt(2)]).kpow(d)
+
+
+def minusi(d: int = 1) -> State:
+  """Product state |-i>."""
+
+  return State([1 / np.sqrt(2), -1j / np.sqrt(2)]).kpow(d)
+
+
+def bitstring(*bits) -> State:
+  """Produce a state from a given bit sequence, eg., |0101>."""
+
+  arr = np.asarray(bits)
+  assert len(arr), 'Need to specify at least 1 qubit'
+  assert ((arr == 1) | (arr == 0)).all(), 'Bits must be 0 or 1'
+
+  t = np.zeros(1 << len(bits), dtype=tensor.tensor_type())
+  t[helper.bits2val(bits)] = 1
+  return State(t)
+
+
+def rand_bits(n: int) -> State:
+  """Produce random combination of |0> and |1>."""
+
+  bits = [random.randint(0, 1) for _ in range(n)]
+  return bitstring(*bits)
+
+
+class Reg(list):
+  """Simple register class, derive from list."""
+
+  def __init__(self, size: int, init=None, global_reg: int = 0) -> None:
+    super().__init__([global_reg + idx for idx in range(size)])
+    self.val = [0] * size
+    if init:
+      if isinstance(init, int):
+        init = format(init, '0{}b'.format(size))
+      if isinstance(init, (str, tuple, list)):
+        for idx, val in enumerate(init):
+          if val == '1' or val == 1:
+            self.val[idx] = 1
+
+  def __str__(self) -> str:
+    return '|' + ''.join([f'{val}' for val in self.val]) + '>'
+
+  def psi(self) -> State:
+    return bitstring(*self.val)
---
+++ example/approximation of single-qubit unitaries/utils/tensor.py
@@ -0,0 +1,135 @@
+"""Implementation of the Tensor base class."""
+
+# This file contains the implementation of the base "tensor" class for all the
+# math in this compiler/simulator. This wrapping is not a unique idea,
+# many open-source implementations wrap a limted set of core numpy
+# functions this way, which should make compilation to, eg., TPU
+# much more straight-forward.
+
+from __future__ import annotations
+import math
+from absl import flags
+import numpy as np
+
+
+# We define the numerical FP bit width with a command-line argument.
+# Usage:
+#   bazel run algorithm -- --tensor_width=128
+#   python3 algorithm.py --tensor_width=128
+#
+# For the interactive use in a Python REPL, it is possible that
+# the absl command-line parser has not yet been called. This is why
+# we bracked tensor_width() in an exception block.
+
+
+flags.DEFINE_integer('tensor_width', 64, 'Bitwidth of FP numbers (64 or 128)')
+
+
+def tensor_width():
+  """Return global floating point bit width."""
+
+  try:  # May be neccessary for interactive use.
+    return flags.FLAGS.tensor_width
+  except Exception:
+    return 64
+
+
+# All vectors/matrices in this package will use this base type.
+# Valid values are np.complex128 or np.complex64
+def tensor_type():
+  """Return complex type based on command-line flag."""
+
+  assert tensor_width() == 64 or tensor_width() == 128
+  return np.complex64 if tensor_width() == 64 else np.complex128
+
+
+class Tensor(np.ndarray):
+  """Tensor is a numpy array representing a state or operator."""
+
+  def __new__(cls, input_array, op_name=None) -> Tensor:
+    cls.name = op_name
+    return np.asarray(input_array, dtype=tensor_type()).view(cls)
+
+  def __array_finalize__(self, obj) -> None:
+    if obj is None:
+      return
+    # np.ndarray has complex construction patterns. Because of this,
+    # if new attributes are needed, this is the place to add them, like this:
+    #    self.info = getattr(obj, 'info', None)
+
+  @property
+  def nbits(self) -> int:
+    return int(math.log2(self.shape[0]))
+
+  def is_close(self, arg, tolerance: float = 1e-6) -> bool:
+    """Check that a 1D or 2D tensor is numerically close to arg."""
+
+    return np.allclose(self, arg, atol=tolerance)
+
+  def is_hermitian(self) -> bool:
+    """Check if this tensor is hermitian - Udag = U."""
+
+    if self.ndim != 2 or self.shape[0] != self.shape[1]:
+      return False
+    return self.is_close(np.conj(self.transpose()))
+
+  def is_unitary(self) -> bool:
+    """Check if this tensor is unitary - Udag*U = I."""
+
+    return Tensor(np.conj(self.transpose()) @ self).is_close(
+        Tensor(np.eye(self.shape[0]))
+    )
+
+  def is_density(self) -> bool:
+    """Check if this tensor is a density operator."""
+
+    if not self.is_hermitian():
+      return False
+    if np.trace(self) > 1.0:
+      return False
+    return True
+
+  def is_pure(self) -> bool:
+    """Check if this tensor describes a pure state (else it is mixed)."""
+
+    if not self.is_density():
+      raise ValueError('ispure() can only be applied to a density matrix.')
+
+    tr_rho2 = np.real(np.trace(self @ self))
+    return np.allclose(tr_rho2, 1.0)
+
+  def is_permutation(self) -> bool:
+    """Check whether a tensor is a true permutation matrix."""
+
+    x = self
+    return (
+        x.ndim == 2
+        and x.shape[0] == x.shape[1]
+        and (x.sum(axis=0) == 1).all()
+        and (x.sum(axis=1) == 1).all()
+        and ((x == 1) | (x == 0)).all()
+    )
+
+  def kron(self, arg: Tensor) -> Tensor:
+    """Return the kronecker product of this object with arg."""
+
+    lhs = self.name if (hasattr(self, 'name') and self.name) else '?'
+    rhs = arg.name if (hasattr(arg, 'name') and arg.name) else '?'
+    return self.__class__(np.kron(self, arg), lhs + '*' + rhs)
+
+  def __mul__(self, arg: Tensor) -> Tensor:  # type: ignore[override]
+    """Inline * operator maps to kronecker product."""
+
+    return self.kron(arg)
+
+  def kpow(self, n: int) -> Tensor:
+    """Return the tensor product of this object with itself `n` times."""
+
+    if n == 0:
+      return self.__class__(1.0)
+    if n == 1:
+      return self.__class__(self, self.name)
+    t = self
+    for _ in range(n - 1):
+      t = np.kron(t, self)
+    return self.__class__(t, (t.name if t.name else '?') + f'^{n}')
